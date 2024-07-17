04f8553b001c2b3929d0b7addeefea487cb79347
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-17 14:13:08 +0200

    Solved - C# - Plus One

---
+++ C#/Easy/Plus One/Program.cs
@@ -0,0 +1,6 @@
+﻿using Plus_One;
+
+Solution task = new();
+
+Console.WriteLine(string.Join(", ", task.PlusOne([4,3,2,1])));
+Console.WriteLine(string.Join(", ", task.PlusOne([9])));
---
+++ C#/Easy/Plus One/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Plus_One;
+
+
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] nextDecade = new int[n + 1];
+        nextDecade[0] = 1;
+        return nextDecade;
+    }
+}
