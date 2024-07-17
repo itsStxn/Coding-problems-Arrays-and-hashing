20bbcf8303c3698e9eb660a55ef841ec967f730e
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-17 14:13:30 +0200

    Solved - C# - Length of Last Word

---
+++ C#/Easy/Length of Last Word/Program.cs
@@ -0,0 +1,4 @@
+﻿using Length_of_Last_Word;
+
+Solution task = new();
+Console.WriteLine(task.LengthOfLastWord("   fly me   to   the moon  "));
---
+++ C#/Easy/Length of Last Word/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace Length_of_Last_Word;
+
+
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        if (s.Length == 1) return 1;
+
+        int charaCount = 0;
+        for (int i = s.Length - 1; i >= 0; i--) {
+            if (s[i] != ' ') charaCount++;
+            else if (charaCount > 0) break;
+        }
+
+        return charaCount;
+    }
+}
