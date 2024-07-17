05653ce2f8d9af32c2efc90bf3e3535378a3f3c4
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-17 14:11:10 +0200

    Solved - C# - Remove Element

---
+++ C#/Easy/Remove Element/Program.cs
@@ -0,0 +1,19 @@
+﻿using Remove_Element;
+
+Solution task = new();
+
+//* CASE 1
+int[] nums = [2,2,2];
+int len = task.RemoveElement(nums, 0);
+
+Console.WriteLine("CASE 1");
+Console.WriteLine(string.Join(", ", nums));
+Console.WriteLine(len);
+
+//* CASE 2
+nums = [0,1,2,2,3,0,4,2];
+len = task.RemoveElement(nums, 2);
+
+Console.WriteLine("\nCASE 2");
+Console.WriteLine(string.Join(", ", nums));
+Console.WriteLine(len);
---
+++ C#/Easy/Remove Element/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Remove_Element;
+
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+        foreach (int n in nums) {
+            if (n != val) nums[k++] = n;
+        }
+
+        return k;
+    }
+}
