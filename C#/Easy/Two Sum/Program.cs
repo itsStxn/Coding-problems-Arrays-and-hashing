6ae79943203c11da4b437b1f8d71f0c03777ceee
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-17 14:10:14 +0200

    Solved - C# - Two Sum

---
+++ C#/Easy/Two Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿using Two_Sum;
+
+Solution task = new();
+int[] res  = task.TwoSum([2,7,11,15], 9);
+Console.WriteLine("[" + string.Join(", ", res) + "]");
---
+++ C#/Easy/Two Sum/Soluiton.cs
@@ -0,0 +1,16 @@
+﻿namespace Two_Sum;
+
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {        
+        Dictionary<int, int> tb = [];
+        for (int i = 0; i < nums.Length; i++) {
+            if (tb.TryGetValue(nums[i], out int val)) {
+                return [val, i];
+            }
+            tb[target - nums[i]] = i;
+        }
+        
+        return default;
+    }
+}
